f31474d00c05fdfe1245c58830b2224d183bd226
Paweł Kątny <pawel.katny@gmail.com>
2022-05-31 21:52:14 +0200

    Add mongodb connection Add config file with mongodb_uri

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const config = require('./config/config');
+
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
 
-app.listen(PORT, () => {
+mongoose.connect(
+    config.MONGODB_URI
+)
+.then(app.listen(config.PORT, () => {
     console.log('Server started at port 3000.');
+}))
+.catch(err => {
+    console.log('Error connecting to DB: ' + err);
 })
